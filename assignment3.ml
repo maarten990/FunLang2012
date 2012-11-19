a7f994bd2202e577fdc4c9dfecffb1cf9114e7e0
Maarten de Jonge <mdejonge1990@gmail.com>
2012-11-19 16:07:21 +0100

    finish first part of assignment 3

--- assignment3.ml
+++ assignment3.ml
@@ -1,8 +1,13 @@
+(*
+ * Functional Languages assignment 3
+ * Maarten de Jonge
+ * Inge Becht
+ * *)
+
 (* Returns whether a given year is a leapyear or not. A leapyear is defined as
  * either:
  * a year after 1582 divisible by 4 but not by 100
  * a year after 1582 divisible by 200
- *
  * Type: int -> bool
  * *)
 let isLeapYear year =
@@ -12,19 +17,19 @@ let isLeapYear year =
     else false
 
 (* Converts a date-tuple to a verbal English string.
- *
  * Type: int * int * int -> string
  * *)
 let date2str (day, month, year) =
     let s_year = string_of_int year in
     let digit  = day mod 10 in
-    let s_day = string_of_int day ^ match digit with
-    | 1 -> "st"
-    | 2 -> "nd"
-    | 3 -> "rd"
-    | _ -> "th" in
-    let months  = ["January"; "Februari"; "March"; "April"; "May"; "April";
-    "June"; "July"; "August"; "September"; "October"; "November"; "December"] in
+    let s_day = string_of_int day ^ if List.mem day [11; 12; 13] then "th" else
+        match digit with
+        | 1 -> "st"
+        | 2 -> "nd"
+        | 3 -> "rd"
+        | _ -> "th" in
+    let months  = ["January"; "February"; "March"; "April"; "May"; "June";
+    "July"; "August"; "September"; "October"; "November"; "December"] in
     let s_month = List.nth months (month - 1) in
     s_month ^ " " ^ s_day ^ ", " ^ s_year
 
@@ -40,7 +45,6 @@ let rec digitSum n =
         lastDigit + digitSum rest
 
 (* Gives the digitroot of the given number.
- *
  * Type: int -> int
  * *)
 let rec digitRoot n =
@@ -50,17 +54,15 @@ let rec digitRoot n =
         digitRoot (digitSum n)
 
 (* Returns the nth element of a list.
- *
- * Type: int -> a list -> a
+ * Type: (int * a list) -> a
  *)
 
-let rec nth n l = match n, l with
+let rec nth (n, l) = match n, l with
     | _, [] -> 0   (* should through an error here or something *)
     | 0, h::_ -> h
-    | n, _::t -> nth (pred n) t
+    | n, _::t -> nth ((pred n), t)
 
 (* Returns a list of the heads of the given list of lists.
- *
  * Type: a list list -> a list)
  *)
 let rec heads l = match l with
@@ -69,35 +71,85 @@ let rec heads l = match l with
     | []::tail -> 0 :: heads tail (* again, error or something *)
 
 (* Zips 2 lists together in tuples of elements.
- *
- * Type: a list -> b list -> a * b list
+ * Type: (a list * b list) -> (a * b) list
  *)
-let rec pairs xs ys = match xs, ys with
+let rec pair (xs, ys) = match xs, ys with
 | [], _ -> []
 | _, [] -> []
-| xh::xt, yh::yt -> (xh, yh) :: pairs xt yt
+| xh::xt, yh::yt -> (xh, yh) :: pair (xt, yt)
 
-
-let rec sublistBegin l m = 
+(* Returns true if l is the starting sequence of m
+ * Type: (a list * a list) -> bool)
+ * *)
+let rec sublistBegin (l, m) = 
     match l, m with
+    | [], [] -> true
     | _, [] -> false
     | [], _ -> true
-    | (lh::lt), (mh::mt) -> if lh = mh then sublistBegin lt mt else false
+    | (lh::lt), (mh::mt) -> if lh = mh then sublistBegin (lt, mt) else false
 
-let rec isSublist l m = 
+(* Returns true if l is a substring of m
+ * Type: (a list * a list) -> bool)
+ * *)
+let rec isSublist (l, m) = 
     match m with
     | [] -> false
-    | mh::mt -> if sublistBegin l m then true else isSublist l mt
+    | mh::mt -> if sublistBegin (l, m) then true else isSublist (l, mt)
 
-(*
+(* Chops the left character off a string
+ * Type: string -> string
+ * *)
+let rec string_lchop s =
+    let rec lchop' s n =
+        if String.length s = n then
+            ""
+        else 
+            (Char.escaped s.[n]) ^ lchop' s (succ n)
+    in lchop' s 1
+
+(* Converts a string into a list of characters.
+ *
+ * Type: string -> char list
+ * *)
 let rec list_of_string s =
-    if String.isempty s then
+    if String.length s = 0 then
         []
     else
-        (String.get s 0) :: list_of_string (String.lchop s)
+        (String.get s 0) :: list_of_string (string_lchop s)
 
-let tokenize c d =
+(* Converts a list of characters into a string.
+ * Type: char list -> string
+ * *)
+let rec string_of_list = function
+    | [] -> ""
+    | h::t -> (Char.escaped h) ^ string_of_list t
+
+
+(* Segments a list of elements c on the given delimiter d)
+ * Type: a -> a list -> a list list
+ * *)
+let rec segment d cs =
+    let rec segment' cs d sub = match cs, d, sub with
+        | [], _, sub -> [sub]
+        | h::t, d, sub -> if h = d then sub::(segment' t d []) else 
+            segment' t d (h::sub)
+    in (segment' cs d [])
+
+(* Tokenizes a list of "words" in the form of character lists, using a list of
+ * delimiters.
+ * Type: a list list -> a list -> a list list
+ * *)
+let rec tokenize' cs ds =
+    match cs, ds with
+    | cs, [] -> cs
+    | cs, d::ds ->
+            let step = List.flatten (List.map (segment d) cs) in
+            tokenize' step ds
+
+(* Tokenizes the string c on the delimiters given in the string ds
+ * Type: string * string -> string list
+ * *)
+let tokenize (c, ds) = 
     let c_list = list_of_string c in
-    let d_list = list_of_string d in
-    4
-*)
+    let ds_list = list_of_string ds in
+    List.map string_of_list (tokenize' [c_list] ds_list)
