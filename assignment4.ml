c8399902a402de921979037a4604e031a251a20f
Maarten de Jonge <mdejonge1990@gmail.com>
2012-11-22 22:52:10 +0100

    add trie type definition

--- assignment4.ml
+++ assignment4.ml
@@ -4,3 +4,7 @@ let rec quicksort = function
         let isSmaller = fun a -> a < x in
         let smaller, largerEq = List.partition isSmaller xs in
         quicksort smaller @ [x] @ quicksort largerEq
+
+(* Defining the trie datatype *)
+type ('a, 'b) trie =
+    | Node of 'a * 'b * (('a, 'b) trie) list
