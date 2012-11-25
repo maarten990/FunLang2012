7b485afaae9d1cddd52d7ccccd232dcaa0dcb822
Maarten de Jonge <mdejonge1990@gmail.com>
2012-11-26 00:01:32 +0100

    implement assignment 11

--- assignment4.ml
+++ assignment4.ml
@@ -41,3 +41,48 @@ type ('a, 'b) trie =
 
     (* A node contains a key, value, and list of children *)
     | Node of 'a * 'b * ('a * ('a, 'b) trie) list
+
+
+
+let rec trie_insert trie key value =
+    match trie with
+    | Node (n_key, _, children) -> Node (n_key, Some value, children)
+
+let rec edge_insert edge keys value =
+    match edge, keys with
+    | (nodekey, trie), [key] when nodekey = key ->
+            (nodekey, (trie_insert trie key (value)))
+
+    | (nodekey, Node (_, nodeval, children)), key::keys when nodekey = key ->
+            let f = fun x -> edge_insert x keys value in
+            (nodekey, Node (nodekey, nodeval, (List.map f children)))
+
+    | (nodekey, Node (_, nodeval, children)), key::keys when nodekey != key ->
+            (nodekey, Node (nodekey, nodeval, children))
+
+
+
+
+(* assignment 11 *)
+(* Types could be added to the operators
+ * (such as IntConst -> IntConst -> IntConst for the arithmetic operatators),
+ * but I'm not sure how useful that would be. *)
+type arithOp = Plus | Minus | Mult | Div | Mod
+type relOp = Eq | Neq | LessThan | LessEq | GreaterThan | GreaterEq
+type logicOp = And | Or
+type monOp = Neg | Not
+type const =
+    | BoolConst of bool
+    | IntConst of int
+
+type binOp =
+    | ArithOp of arithOp
+    | RelOp of relOp
+    | BinOp of binOp
+
+type expr =
+    | Expr of expr
+    | BinRel of expr * binOp * expr
+    | MonRel of monOp * expr
+    | Id
+    | Const of const
