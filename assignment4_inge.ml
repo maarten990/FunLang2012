197561fa022e204f5a63be44e8ccd1ddf232399e
Inge <inge.becht91@gmail.com>
2012-11-25 18:32:40 +0100

    almost there

--- assignment4_inge.ml
+++ assignment4_inge.ml
@@ -5,20 +5,22 @@ type ('a, 'b) trie =
 
     (* A node contains a key, value, and list of children *)
     | Node of 'a * 'b * ('a * ('a, 'b) trie) list
-    
+
+
 let rec insert' nodes key value = 
     match nodes with
     (* in case no more nodes add to layer*)
-    | [] -> (key, Node(key, value, []))
+    | [] -> [insert_value key value]
+    | (h, Node(foo, bar, children))::t when h = (List.hd key) ->
+              (h, Node(foo, bar, (insert' children (List.tl key) value)))::t
+    | h::t -> h::(insert' t key value)
 
 
-let rec insert_value nodes key value = 
-    if List.length key = 1 then (key, Node (key, value, []))
+let rec insert_value key value = 
+    if (List.length key) = 1 then (List.hd key, Node (List.hd key, value, []))
     else
-        (key, Node(List.hd key, None, (insert_value nodes (List.tl key) value)))
-
+        (List.hd key, Node(List.hd key, None,  [insert_value (List.tl key) value]))
 
 let insert trie key value = 
     match trie with 
     | Root(nodes) -> Root(insert' nodes key value)
-
